4fa7ed2b62a3fd613b1d4e94fa2d6d65530e5ef9
Printassia Bey <printassiajohnson@gmail.com>
2020-08-28 08:00:55 -0400

    Update and rename Exercise4_12.java to Exercise4_12HexToBinary.java

--- Exercise4_12HexToBinary.java
+++ Exercise4_12HexToBinary.java
@@ -2,17 +2,16 @@ package Horsy.com.company;
 
 import java.util.Scanner;
 
+/*Write a program that prompts the user to enter a hex digit and displays its corresponding binary number.  
+*/
+
 public class Exercise4_12 {
-    //Hex To Binary pg:176
+    //Hex To Binary 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a hex digit: ");
         String hexDigit = input.next().toUpperCase();
         char character = hexDigit.charAt(0);
-
-
-
-        // System.out.println("The binary value is " + binaryValue);
 
         if (character < 'A' || character > 'F')
             System.out.println(hexDigit + " is an invalid input ");
