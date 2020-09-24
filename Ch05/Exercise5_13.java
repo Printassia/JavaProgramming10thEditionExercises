1cd65f521792793944902f77609c298d905e49c0
Printassia Bey <printassiajohnson@gmail.com>
2020-09-24 12:35:03 -0400

    Add files via upload

--- Ch05/Exercise5_13.java
+++ Ch05/Exercise5_13.java
@@ -8,10 +8,10 @@ public class Exercise5_13 {
     public static void main(String[] args) {
         int number = 0;
         //If the number cubed is less than 12000 keep increasing the number
-        while (Math.pow(number, 3) < 12000){
+        while(Math.pow(number + 1, 3) < 12000){
             number++;
         }
         System.out.println(number + " is the largest integer n, " +
-                "such that n^3 is greater than 12000.");
+                "such that n^3 is less than 12000.");
     }
 }
