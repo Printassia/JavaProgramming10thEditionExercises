acd5d55b1ad1eadd3d7a32c4eb0a3727ed650554
Printassia Bey <printassiajohnson@gmail.com>
2020-09-24 11:17:02 -0400

    Update Exercise5_11.java

--- Ch05/Exercise5_11.java
+++ Ch05/Exercise5_11.java
@@ -1,6 +1,11 @@
 package Horsy.com.company;
 
 public class Exercise5_11 {
+    /*
+    (Find numbers divisible by 5 or 6, but not both) Write a program that displays
+    all the numbers from 100 to 200, ten per line, that are divisible by 5 or 6, but not
+    both. Numbers are separated by exactly one space
+     */
     public static void main(String[] args) {
         int count = 0;
         final int NUMBERS_PER_LINE = 10;
