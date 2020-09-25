6aa606f169c52730d8e7f287b0d61c307b099a64
Printassia Bey <printassiajohnson@gmail.com>
2020-09-25 08:37:34 -0400

    Update Exercise5_13.java

--- Ch05/Exercise5_13.java
+++ Ch05/Exercise5_13.java
@@ -7,7 +7,7 @@ public class Exercise5_13 {
      */
     public static void main(String[] args) {
         int number = 0;
-        //If the number cubed is less than 12000 keep increasing the number
+        //number cubed is less than 12000
         while(Math.pow(number + 1, 3) < 12000){
             number++;
         }
