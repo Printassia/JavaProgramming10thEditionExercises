3464b7bf3fd49ec91aab75a926328cbfcf804c9b
Printassia Bey <printassiajohnson@gmail.com>
2020-10-03 04:10:55 -0400

    Update Exercise5_22.java

--- Ch05/Exercise5_22.java
+++ Ch05/Exercise5_22.java
@@ -2,6 +2,15 @@ package Horsy.com.company;
 
 import java.util.Scanner;
 
+/*
+(Financial application: loan amortization schedule) The monthly payment for a
+given loan pays the principal and the interest. The monthly interest is computed
+by multiplying the monthly interest rate and the balance (the remaining principal).
+The principal paid for the month is therefore the monthly payment minus
+the monthly interest. Write a program that lets the user enter the loan amount,
+number of years, and interest rate and displays the amortization schedule for the loan.
+ */
+
 public class Exercise5_22 {
     public static void main(String[] args) {
 
