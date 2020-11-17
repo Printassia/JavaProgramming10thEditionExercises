1fbeaa06fc1d01a586380f71b3c7496ab5acb81b
Printassia Bey <printassiajohnson@gmail.com>
2020-11-17 08:54:03 -0500

    first commit

---
+++ Ch06/Exercise6_35AreaOfAPentagon.java
@@ -0,0 +1,21 @@
+package Horsy.com.company.Ch06;
+
+import java.util.Scanner;
+
+public class Exercise6_35AreaOfAPentagon {
+    public static double area(double side){
+        double area = 5 * side * side / (4 * (Math.tan((Math.PI / 5))));
+        return area;
+    }
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+
+       System.out.print("Enter the side: ");
+       double side = input.nextDouble();
+
+       //Display
+        System.out.println("The area of the pentagon is " + area(side));
+
+    }
+
+}
